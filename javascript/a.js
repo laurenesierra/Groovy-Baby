da6f5dbbb2d17bae84e5d483967c17ad3e9a7c07
Lauren Sierra <laurenesierra@gmail.com>
2020-11-13 12:58:18 -0800

    merge

--- javascript/a.js
+++ javascript/a.js
@@ -1,8 +1,10 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+function displayGreeting(){
 
-if (hourNow > 18) {
+let today = new Date();
+let hourNow = today.getHours();
+let greeting;
+
+if (hourNow > 18){
         greeting = 'Heyyoo';
     } else if (hourNow > 12) {
         greeting = 'Hows it sitting?!';
@@ -13,3 +15,32 @@ if (hourNow > 18) {
     }
 
     document.write('<h3>' + greeting + '</h3');
+}
+
+function showPopUp(){
+
+alert("allow myself to introduce...myself");
+}
+
+function displayButton(){
+
+
+document.getElementById('11').style ='color:orange';
+}
+
+function password(){
+    let answer = '';
+    while (answer !== '4') {
+        answer = prompt ('Hey there! What is 2+2?');
+    }
+
+}
+
+
+
+password();
+showPopUp();
+displayGreeting();
+
+
+document.getElementById('11').addEventListener('click',displayButton);
--- javascript/b.js
+++ javascript/b.js
@@ -1,1 +1,21 @@
-alert("allow myself to introduce...myself");
+function displayImage(){
+
+    let correctAnswer = orange;
+
+
+    for (let i = 0; i < numberOfGuesses; i = i + 1){
+        let userAnswer = prompt('Pick a number between 1 and 5?:');
+        if(userAnswer > 0 && userAnswer < 5){
+         
+                
+             
+     
+    
+    
+   
+            document.images('<img src="https://ih1.redbubble.net/image.862633273.0435/st,small,507x507-pad,600x600,f8f8f8.u1.jpg">');;
+        } 
+    
+        document.write('<h3>' + greeting + '</h3');
+
+
